f3f774e63bfb45a7b9245d5b4ae6d76c7569e940
MatheusValera <lrfp1000@.gmail.com>
2024-01-08 18:38:28 -0300

    refactor: change PostgresHelper

--- src/infra/postgres/PostgresHelper.ts
+++ src/infra/postgres/PostgresHelper.ts
@@ -3,7 +3,7 @@ import { Pool } from 'pg'
 import * as dotenv from 'dotenv'
 dotenv.config({ path: path.join(__dirname, '../../../.env') })
 
-const pool = new Pool({
+const poolPostgres = new Pool({
   host: process.env.DW_POSTGRES_HOST,
   user: process.env.DW_POSTGRES_USER,
   database: process.env.DW_POSTGRES_DB,
@@ -12,10 +12,10 @@ const pool = new Pool({
   port: 5432
 })
 
-export const dwdb = {
-  query: (text, params) => pool.query(text, params)
+export const PostgresHelper = {
+  query: (text, params) => poolPostgres.query(text, params)
 }
 
 export const getClient = async (): Promise<any> => {
-  return pool.connect()
+  return poolPostgres.connect()
 }
