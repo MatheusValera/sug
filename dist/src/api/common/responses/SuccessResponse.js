65232c24ac5aa8029c35b93b48d9738166918be6
MatheusValera <lrfp1000@.gmail.com>
2024-03-14 08:38:26 -0300

    chore: remove dist

--- dist/src/api/common/responses/SuccessResponse.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SuccessResponse = void 0;
-class SuccessResponse {
-    static async handler(response, body) {
-        return response.status(200).json(body);
-    }
-}
-exports.SuccessResponse = SuccessResponse;
--- dist/src/domain/data/entity/ISchedule.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EStatus = void 0;
-var EStatus;
-(function (EStatus) {
-    EStatus["active"] = "active";
-    EStatus["inactive"] = "inactive";
-})(EStatus = exports.EStatus || (exports.EStatus = {}));
